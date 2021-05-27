b207890a86ee2445d9250de0ad9d916a6ef983ea
Rıdvan <ridvanabay@gmail.com>
2021-05-28 00:57:56 +0300

    Added Core Layer - Result

---
+++ Core/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+   public class Result
+    {
+        public string Message { get; set; }
+        public bool Success { get; set; }
+
+        public class GetOneResult<TEntity>:Result
+            where TEntity:class,new()
+        {
+            public TEntity Entity { get; set; }
+        }
+
+        public class GetManyResult<TEntity> : Result
+           where TEntity : class, new()
+        {
+            public IEnumerable<TEntity> Entity { get; set; }
+        }
+    }
+}
