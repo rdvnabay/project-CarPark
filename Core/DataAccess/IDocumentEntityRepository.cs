e8053d3111789c35ed8aa27116df7c025c83b07c
Rıdvan <ridvanabay@gmail.com>
2021-05-28 02:33:53 +0300

    Added MongoConnectiınSettings Added Interface IEntity and IDto Added MongoDb for DocumentDbEntitiy

--- Core/DataAccess/IDocumentEntityRepository.cs
+++ Core/DataAccess/IDocumentEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using Core.Entities;
+using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 namespace Core.DataAccess
 {
     public interface IDocumentEntityRepository<T>
+        where T:DocumentDbEntity
     {
         void Add(T entity);
         Task AddAsyc(T entity);
---
+++ Core/DataAccess/MongoDb/Configuration/MongoConnectionSettings.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+namespace Core.DataAccess.MongoDb.Configuration
+{
+    public class MongoConnectionSettings
+    {
+        private MongoClientSettings MongoClientSettings { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+        public MongoConnectionSettings(MongoClientSettings mongoClientSettings)
+        {
+            MongoClientSettings = mongoClientSettings;
+        }
+
+        public MongoConnectionSettings()
+        {
+
+        }
+
+        public MongoClientSettings GetMongoClientSettings()
+        {
+            return MongoClientSettings;
+        }
+    }
+}
