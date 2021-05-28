352a8bde3d649f32842a35e2885f9ea6cd97f312
Rıdvan <ridvanabay@gmail.com>
2021-05-28 15:08:47 +0300

    Added MongoDbContext Added MongoDbConnection appsettings.json

---
+++ DataAccess/Concrete/MongoDb/Context/MongoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.MongoDb.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.Concrete.MongoDb.Context
+{
+    public abstract class MongoDbContext
+    {
+        public readonly IConfiguration Configuration;
+        public readonly MongoConnectionSettings MongoConnectionSettings;
+
+        protected MongoDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            MongoConnectionSettings = configuration.GetSection("MongoDbSettings") as MongoConnectionSettings;
+        }
+    }
+}
